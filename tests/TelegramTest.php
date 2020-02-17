39056cd5aaacf08ab60009c036d0f450fbc59bba
alexts <spacecv@yandex.ru>
2020-02-17 06:57:07 +0300

    complete

---
+++ tests/TelegramTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Telegram\Bot\Api;
+use Telegram\Bot\Objects\Message;
+
+class TelegramTest extends TestCase
+{
+    protected $telegram;
+
+    public function setUp()
+    {
+        $this->telegram = new Api('token');
+    }
+
+    /** @test */
+    public function sendMessage()
+    {
+        $this->telegram = new Api(env('TELEGRAM_BOT_TOKEN', ''));
+
+        $var = [
+            'chat_id'     => env('TELEGRAM_CHAT_ID', ''),
+            'text'        => 'OK',
+        ];
+
+        $response = $this->telegram->sendMessage(['chat_id' => $var['chat_id'], 'text' => $var['text']]);
+
+        $this->assertInstanceOf(Message::class, $response);
+        $this->assertEquals($var['chat_id'], $response->getChat()->getId());
+        $this->assertEquals($var['text'], $response->getText());
+    }
+}
